a9258f2aa5303f67a50092aa11988dfa2080841c
Sai Sarath <99417849+sarath200313@users.noreply.github.com>
2022-06-30 22:19:41 +0530

    Add files via upload

---
+++ WW1 4TH.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    if (i=0,2,3)
+        printf("Rani Lakshmi Bai\n");
+    else
+        printf("Niveditha\n");
+    printf("%d\n",i);
+}
---
+++ WW1 5TH.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	if (-5)
+	{
+		printf("a");
+		printf("b");
+
+	}
+	else
+	{
+		printf("c");
+		printf("d");
+	}
+	return 0;
+}
+
